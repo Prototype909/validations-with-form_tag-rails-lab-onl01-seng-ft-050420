c7d0c634f4a4774f911fa0f2dcc146cda4f41363
Steve Anderson <prototype909@msn.com>
2020-07-16 00:15:48 +0000

    Automatically backed up by Learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,6 +1,10 @@
 class Author < ActiveRecord::Base
   validates :name, presence: true
+<<<<<<< HEAD
   #validates :name, length: {minimum: 2}
+=======
+  validates :name, length: {minimum: 2}
+>>>>>>> 7b1a472f7a94d950a441dcf3faa63c32c09cb6ea
   validates :email, uniqueness: true
   validates :phone_number, length: {is: 10}
 end
